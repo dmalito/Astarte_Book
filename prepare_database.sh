20ff97b578c427391ae1fb5538195b24f0294c7e
Davide Malito <davide.malito@cern.ch>
2024-02-25 19:17:24 +0000

    add function to change special characters

--- prepare_database.sh
+++ prepare_database.sh
@@ -2,12 +2,32 @@
 # Define the source directory
 source_directory="./source/"
 
+# Function to remove accents from a string
+remove_accents() {
+    echo "$1" | iconv -f UTF-8 -t ASCII//TRANSLIT
+}
+
+# Iterate over each file in the directory
+for file in "$source_directory"/*; do
+    # Extract the filename without the directory path
+    filename=$(basename "$file")
+    
+    # Remove accents from the filename
+    new_filename=$(remove_accents "$filename")
+    
+    # Rename the file if there's a change in the filename
+    if [[ "$filename" != "$new_filename" ]]; then
+        mv "$file" "$source_directory/$new_filename"
+        echo "Renamed '$filename' to '$new_filename'"
+    fi
+done
+
 #change all teh sapces to underscores
 rename --nows ${source_directory}*.md
 rename --nows ${source_directory}*.csv
 
 # delete all the unwanted files
-files_to_delete="Acrobazia_ Atletica_ Furtività_ Rapidità_di_mano_ Religione_ Destrezza_ Forza_ Intelligenza_ Saggezza_ Carisma_"
+files_to_delete="Acrobazia_ Atletica_ Furtivit\`a_ Rapidit\`a_di_mano_ Religione_ Destrezza_ Forza_ Intelligenza_ Saggezza_ Carisma_"
 echo "Deleting files: $files_to_delete"
 for file in $files_to_delete; do
     rm ${source_directory}/${file}*md
