8975d7337c4be98dc8e18355b6e653f65a604e52
Davide Malito <davide.malito@cern.ch>
2024-02-25 18:47:19 +0000

    update script to create the lists

--- create_lists.sh
+++ create_lists.sh
@@ -4,8 +4,11 @@ input_files=$(ls source/Lista*.csv)
 for file in $input_files
 do
     echo "Processing $file"
-    output_file=$(echo $file | sed 's/.csv/.tex/')
-    echo " " > ${output_file}
-    cat $file | awk -F, '{print "\\input{} " $1 "}"}' >> $output_file
+    output_file=$(echo $file | sed 's/source/./' | sed 's/.csv/.tex/')
+    echo "" > ${output_file}
+    cat $file | awk -F, '{print "\\input{source/" $1 "}"}' >> $output_file
+    #delete first two lines in the file
+    sed -i'' -e '1,2d' $file
+    #substitue every space with underscore
+    sed -i'' -e  's/ /_/g' $file
 done
-```
