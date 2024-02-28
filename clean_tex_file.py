641da5aa5b96d4fb84e1f12b4c7dcce37155677a
Davide Malito <davide.malito@cern.ch>
2024-02-28 23:42:43 +0000

    Migliorie generali. Aggiunte funzioni per spostare le immagini nel testo

--- clean_tex_file.py
+++ clean_tex_file.py
@@ -13,81 +13,72 @@ args = parser.parse_args()
 
 file_name = parser.parse_args().input
 
-
 # Function to filter lines based on patterns
 def filter_lines(lines, patterns):
     return [line for line in lines if not any(pattern in line for pattern in patterns)]
+
+patterns_to_remove = [ # Remove lines containing any of these strings
+    '\\begin{center}\\rule',
+    'XP attuali',
+    'Descrizione Generale',
+    'caption',
+    'Età:', 'Razza:', 'Relazioni:', 'Alleati:', 'Nemesi:', 'Possedimenti importanti:', 'Tags:', 'Livello:', 'Info generali', 'Classe:', 'Alias:', 'di nascita:', 'Informazioni Generali',
+]
+patterns_to_edit = [ # Replace the first string with the second string
+    ('.webp', '.jpeg'),  
+    #('begin{figure}', 'begin{figure}[h!]'),
+    ('begin{figure}', 'begin{wrapfigure}{r}{.4\\textwidth}'),
+    ('end{figure}', 'end{wrapfigure}'),
+    ('includegraphics', 'includegraphics[width=0.4\\textwidth]'),
+]
+strings_delete_after = [ # Delete the lines after the first instance of any of these strings
+    r'\subsection{A.',
+    r'Coinvolgimenti in'
+]
 
 # Read the file content
 with open(file_name, 'r') as file:
     lines = file.readlines()
 
-# Define patterns for lines to be removed or modified
-patterns_to_remove = [
-    '\\begin{center}\\rule',
-    'XP attuali',
-]
-patterns_to_edit = [
-    ('.webp', '.jpeg'),  # Change .webp to .jpeg
-    #('begin{figure}', 'begin{figure}[h!]') # Add [h!] to \begin{figure}
-    #(r'\d+(\.\d+)* ', ''),  # Delete all the characters line number and number.number
-]
-
 # Filter lines based on patterns
 lines = filter_lines(lines, patterns_to_remove)
 
-# Delete lines from 2 to 6 only if line 3 contains "Tags:"
-#if any("Tags:" in line for line in lines[2:3]):
-#    del lines[1:6]
-
 # delete lines from 2 to the next instance of "\section{, including the line with "\section{
-# "Tags
+# DM probabilmente si può fare in modo più efficiente
 if any(r'\section{' in line for line in lines[2:]):
     end_index = next((i for i, line in enumerate(lines[2:]) if r'\section{' in line), None)
     if end_index is not None:
         del lines[1:end_index+3]
 
-# Delete lines from the line containig "Informazioni Generali" to the next instance of "\section{" or "\subsection{"
-start_index = next((i for i, line in enumerate(lines) if r'Informazioni Generali' in line), None)
-if start_index is not None:
-    end_index = next((i for i, line in enumerate(lines[start_index:]) if r'\section{' in line or r'\subsection{' in line), None)
-    if end_index is not None:
-        del lines[start_index:start_index+end_index]
         
-# Delete lines from the line containig "Info generali" to the next instance of "\section{" or "\subsection{"
-start_index = next((i for i, line in enumerate(lines) if r'Info generali' in line), None)
-if start_index is not None:
-    end_index = next((i for i, line in enumerate(lines[start_index:]) if r'\section{' in line or r'\subsection{' in line), None)
-    if end_index is not None:
-        del lines[start_index:start_index+end_index]
-
 # if an open { down't have a close }, merge with the next lines until the close }
 for i, line in enumerate(lines):
     if line.count('{') > line.count('}'):
-        lines[i] = line.strip() + lines[i+1].strip()
+        lines[i] = line.strip() + ' ' + lines[i+1].strip()
         lines[i+1] = ''
         if lines[i].count('{') > lines[i].count('}'):
             lines[i] = line.strip() + lines[i+2].strip()
             lines[i+2] = ''
-
-
-
-## remove the numbers in front of the name
+            
+#find lines containig "texorpdfstring" and extract the name of the section, subsection or subsubsection
 for i, line in enumerate(lines):
-    # put all the content enc
-    if "texorpdfstring" in line:
-        print(f"line {i}: {line}")
-        content = re.findall(r'\{(.*?)\}', line)[-2]
-        if "\\subsection{" in line:
-            newline = "\\subsection{"+content+"}"
-        elif "\\subsubsection{" in line:
-            newline = "\\subsubsection{"+content+"}"
-        lines[i] = newline
-#        start_index = line.find('{') + 1
-#        end_index = line.find('}', start_index)
-#        content_within_braces = line[start_index:end_index]
+    if r'texorpdfstring' in line:
+        # Extract content within \textbf{ and }
+        start_index = line.find('\\textbf{')
+        end_index = line.find('}', start_index)
+        content_within_braces = line[start_index+8:end_index]
+        # Extract content within \label{ and }
+        start_index = line.find('\\label{') 
+        end_index = line.find('}', start_index)
+        label = line[start_index:end_index+1]
+        if "subsubsection" in line:
+            lines[i] = '\\subsubsection{'+ content_within_braces + '}'+label+'\n'
+        elif "subsection" in line:
+            lines[i] = '\\subsection{'+ content_within_braces + '}'+label+'\n'
+        elif "section" in line:
+            lines[i] = '\\section{'+ content_within_braces + '}'+label+'\n'
 
-# remove the numbers in front of the name
+# remove the numbers in front of the section names
 for i, line in enumerate(lines):
     if "\\subsection{" in line or "\\subsubsection{" in line:
         # Extract content within {}
@@ -101,48 +92,61 @@ for i, line in enumerate(lines):
             lines[i] = line[:start_index] + new_content + line[end_index:]
 
 
-# Delete all the lines after "\subsection{5. Coinvolgimenti in eventi"
-start_index = next((i for i, line in enumerate(lines) if r'\subsection{5. Coinvolgimenti in' in line), None)
-if start_index is not None:
-    del lines[start_index:]
-start_index = next((i for i, line in enumerate(lines) if r'\subsection{A.' in line), None)
-if start_index is not None:
-    del lines[start_index:]
-
-
-
-# delete all lines after the one containing "Coinvolgimenti in eventi"
-start_index = next((i for i, line in enumerate(lines) if (r'Coinvolgimenti in' in line and r'\subsection{' in line)), None)
-if start_index is not None:
-    del lines[start_index:]
-
-# delete the lines between \begin{figure} and \end{figure}, but only the second time they appear
-start_index = next((i for i, line in enumerate(lines) if r'\begin{figure}' in line), None)
-if start_index is not None:
-    end_index = next((i for i, line in enumerate(lines[start_index:]) if r'\end{figure}' in line), None)
-    if end_index is not None:
-        end_index = end_index + start_index
-        start_index = next((i for i, line in enumerate(lines[end_index:]) if r'\begin{figure}' in line), None)
-        if start_index is not None:
-            start_index = start_index + end_index
-            end_index = next((i for i, line in enumerate(lines[start_index:]) if r'\end{figure}' in line), None)
-            if end_index is not None:
-                end_index = end_index + start_index
-                print(f"In file {file_name}, deleting lines from {start_index} to {end_index}")
-                del lines[start_index:end_index+1]
-
-# delete lines that contain "\caption"
+# delete the lines after the first instance of a string in strings_delete_after
+start_index = None
 for i, line in enumerate(lines):
-    if "caption" in line:
-        lines[i] = ''
+    for string in strings_delete_after:
+        if string in line:
+            start_index = i
+            break
+    if start_index is not None:
+        break
+if start_index is not None:
+    del lines[start_index:]
+
+
+# gather all the lines enclosed between \begin{figure} and \end{figure}
+# keep only the first figure and delete the others
+figures_list = []
+for i, line in enumerate(lines):
+    if r'\begin{figure}' in line:
+        start_index = i
+    if r'\end{figure}' in line:
+        end_index = i
+        figure_lines = lines[start_index:end_index+1]
+        # delete the lines between \begin{figure} and \end{figure}
+        del lines[start_index:end_index+1]
+        # add the figure lines to the end of the file
+        figures_list.append(figure_lines)
+if len(figures_list) > 1:
+    del figures_list[1:]
+    
+    
+#append the figure lines after the first instance of \subsection if the next section is not \subsubsection
+for i, line in enumerate(lines):
+    if r'\subsection{' in line:
+        start_index = i
+        #if any of the next 5 lines contains \subsubsection, then append the figure lines after the first instance of \subsubsection
+        if any(r'\subsubsection{' in line for line in lines[start_index:start_index+5]):
+            #get the index of the first \subsubsection
+            start_index = next((i for i, line in enumerate(lines[start_index:]) if r'\subsubsection{' in line), None)
+            if start_index is not None:
+                start_index = start_index + i + 1
+                lines[start_index:start_index] = figures_list[0]
+                break
+            else:
+                break
+        else:
+            lines[start_index:start_index] = figures_list[0]
+            break
 
 # Apply modifications to lines based on patterns
 for i, line in enumerate(lines):
     for pattern, replacement in patterns_to_edit:
         lines[i] = lines[i].replace(pattern, replacement)
+
         
-#outfile = file_name.replace(".tex", "_cleaned.tex")
-outfile = file_name
+outfile = file_name.replace("source_raw", "source")
 # Write the modified content back to the file
 with open(outfile, 'w') as file:
     file.writelines(lines)
